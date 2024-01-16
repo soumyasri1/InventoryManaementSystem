57fd994b9285547b4f9bae941e93abfab2ca377d
soumyasri1 <31533704+soumyasri1@users.noreply.github.com>
2024-01-16 08:22:43 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
@@ -10,7 +8,6 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-   <ToastContainer />
    </>
 );
 
